33769c02e65012e716d6258140d8b493cdf9e617
vewong <medras13@gmail.com>
2014-07-25 18:02:27 -0700

    Artapalooza!
    Forgot to update over the past week where I was just working on art.
    Sheep sprite, sheep animations, ground, fence, and sky were all added.

--- Assets/Scripts/SheepJump.cs
+++ Assets/Scripts/SheepJump.cs
@@ -3,22 +3,28 @@ using System.Collections;
 
 public class SheepJump : MonoBehaviour {
 
+    Animator anim;
+    
     public bool jumping = false;
     public Transform groundCheck;
     float groundCheckRadius = .9f;
     public LayerMask whatIsGround;
-    float jumpHeight = 400f;
+    float jumpHeight = 600f;
     float moveSpeed = 3f;
 
 	// Use this for initialization
 	void Start () 
     {
         this.rigidbody2D.velocity = new Vector2(moveSpeed, this.rigidbody2D.velocity.y);
+
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         jumping = !(Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround));
+
+        anim.SetBool("jumping", jumping);
 	}
 
     void Update()
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerController : MonoBehaviour {
+
+    float timeRemaining = 30f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //InvokeRepeating("decreaseRemainingTime", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //first check if the game is over, then subtract the time passed since last update if the game isn't over
+	    if (timeRemaining <= 0)
+        {
+            SendMessageUpwards("TimeHasElapsed");
+        }
+        else
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+	}
+
+    void OnGUI()
+    {
+        if (timeRemaining > 10)
+        {
+            GUI.Box(new Rect(10, 0, 50, 50), ":" + (int)timeRemaining);
+        }
+        else
+        {
+            GUI.Box(new Rect(10, 0, 50, 50), ":0" + (int)timeRemaining);
+        }
+    }
+}
